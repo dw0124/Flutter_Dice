c84d4bea89fd7a3c1780b4a3e07594e140cdba1a
dw0124 <doowone8650@naver.com>
2025-04-03 16:55:27 +0900

    로그인 페이지 UI 구현 SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +36,66 @@ class LogIn extends StatelessWidget {
               onPressed: (){}
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child:
+                Image(image: AssetImage('image/chef.gif'),
+                  width: 170.0,
+                  height: 190.0,
+              ),
+            ),
+            Form(
+                child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(color: Colors.teal, fontSize: 15.0),
+                      )
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Enter dice'
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Enter Password'
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 40.0,),
+                          ButtonTheme(
+                            minWidth: 100.0,
+                            height: 50.0,
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.orangeAccent, // 버튼의 최소 크기
+                                  ),
+                                  onPressed: () {},
+                                  child: Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.white,
+                                      size: 35.0,
+                                    ),
+                                  )
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+            ),
+          ],
+        ),
       ),
     );
   }
